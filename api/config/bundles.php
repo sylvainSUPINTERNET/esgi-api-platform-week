4d2845b311ba300f86e73292c41966924696874a
sylvainSUPINTERNET <sylvain.joly00@orange.fr>
2020-05-13 10:18:34 +0200

    Revert "Decode token"

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -11,7 +11,4 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true]
 ];
--- api/src/Entity/Greeting.php
+++ api/src/Entity/Greeting.php
@@ -9,6 +9,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * This is a dummy entity. Remove it!
  *
+ * @ApiResource
  * @ORM\Entity
  */
 class Greeting
