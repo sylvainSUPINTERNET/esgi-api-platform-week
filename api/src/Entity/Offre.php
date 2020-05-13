9d485169b58b4337365c4c79d40bc99071a8c0f5
Thomas <thomas.mallet45@gmail.com>
2020-05-13 14:01:10 +0200

    update entity offre

--- api/src/Entity/Offre.php
+++ api/src/Entity/Offre.php
@@ -4,11 +4,29 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\OneToMany;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
- * @ApiResource()
- * @ORM\Entity(repositoryClass="App\Repository\OffreRepository")
+ *   @ApiResource(
+ *     normalizationContext={"groups"={"get"}},
+ *     itemOperations={
+ *         "get",
+ *         "put"={
+ *             "normalization_context"={"groups"={"put"}},
+ *             "denormalization_context"={"groups"={"put_in"}}
+ *         }
+ *    },
+ *    collectionOperations={
+ *         "post"={
+ *             "normalization_context"={"groups"={"post"}},
+ *             "denormalization_context"={"groups"={"post_in"}},
+ *             "security"="is_granted('ROLE_RECRUTEUR')"
+ *         }
+ *     }
+ * )
+ * @ORM\Entity(repositoryClass="App\Repository\OfferRepository")
  */
 class Offre
 {
@@ -16,46 +34,45 @@ class Offre
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"get"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"get", "put", "put_in", "post_in"})
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"get", "put", "put_in", "post_in"})
      */
     private $description;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"get", "put", "put_in", "post_in"})
      */
     private $companyDescription;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"get", "put", "put_in", "post_in"})
      */
     private $startAt;
 
     /**
      * @ORM\Column(type="string", length=255)
+    * @Groups({"get", "put", "put_in","post","post_in"})
      */
     private $workingPlace;
 
-
     /**
      * @ORM\OneToMany(targetEntity="Apply", mappedBy="offre")
+     * @Groups({"get", "put", "put_in","post"})
      */
     private $applies;
-
-
-    /**
-     * @ORM\ManyToOne(targetEntity="User", inversedBy="offres")
-     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
-     */
-    private $user;
 
     public function getId(): ?int
     {
@@ -142,21 +159,5 @@ class Offre
             $apply->setOffer($this);
         }
         return $this;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
-
-    /**
-     * @param mixed $user
-     */
-    public function setUser($user): void
-    {
-        $this->user = $user;
     }
 }
