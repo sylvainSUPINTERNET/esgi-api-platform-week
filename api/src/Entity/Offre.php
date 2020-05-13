862b51b86231523766b4b62883395faad38d363c
Thomas <thomas.mallet45@gmail.com>
2020-05-13 14:14:24 +0200

    update entity offre

--- api/src/Entity/Offre.php
+++ api/src/Entity/Offre.php
@@ -4,11 +4,32 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
 
 /**
- * @ApiResource()
- * @ORM\Entity(repositoryClass="App\Repository\OffreRepository")
+ *   @ApiResource(
+ *     normalizationContext={"groups"={"get"}},
+ *     itemOperations={
+ *         "get",
+ *         "put"={
+ *             "normalization_context"={"groups"={"put"}},
+ *             "denormalization_context"={"groups"={"put_in"}}
+ *         }
+ *    },
+ *    collectionOperations={
+ *         "post"={
+ *             "normalization_context"={"groups"={"post"}},
+ *             "denormalization_context"={"groups"={"post_in"}},
+ *             "security"="is_granted('ROLE_RECRUTEUR')"
+ *         }
+ *     }
+ * )
+ * @ORM\Entity(repositoryClass="App\Repository\OfferRepository")
  */
 class Offre
 {
@@ -16,37 +37,44 @@ class Offre
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"get"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"get", "put", "put_in", "post_in"})
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"get", "put", "put_in", "post_in"})
      */
     private $description;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"get", "put", "put_in", "post_in"})
      */
     private $companyDescription;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"get", "put", "put_in", "post_in"})
      */
     private $startAt;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"get", "put", "put_in","post","post_in"})
      */
     private $workingPlace;
 
 
     /**
      * @ORM\OneToMany(targetEntity="Apply", mappedBy="offre")
+     * @Groups({"get", "put", "put_in","post"})
      */
     private $applies;
 
@@ -54,6 +82,7 @@ class Offre
     /**
      * @ORM\ManyToOne(targetEntity="User", inversedBy="offres")
      * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     * @Groups({"get", "put", "post", "post_in"})
      */
     private $user;
 
